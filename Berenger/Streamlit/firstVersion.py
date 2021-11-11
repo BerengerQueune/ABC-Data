bc191549b9e60848e714ad9f0886773001d92e4d
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-11 18:44:17 +0100

    Update

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -7,14 +7,29 @@ import datetime as dt
 import plotly.express as px
 import ipywidgets as widgets
 
-df = pd.read_csv("https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Streamlit/table_finale.csv?token=AU6BUZUYDW7RGQADLHKLGQDBSZ2OO")  # read a CSV file inside the 'data" folder next to 'app.py'
-# df = pd.read_excel(...)  # will work for Excel files
-
 st.title("Hello world!")  # add a title
-st.write(df)  # visualize my dataframe in the Streamlit app
 
 acteur_par_periode = pd.read_csv("https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Streamlit/acteur_par_periode.csv?token=AU6BUZWYJ6GYLJLQVDQCLZTBSZ2NK")
+link = 'https://raw.githubusercontent.com/BerengerQueune/ABC-Data/44cd0ac3e7485b09deb72315afb46baf0ea614fc/Christophe/top10.csv?token=AVCI5T6ITPAUVMV6XERVCEDBSU6SC'
+top10 = pd.read_csv(link)
+presence_acteur = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/B%C3%A9renger/Database_projet/presence_acteurs.csv?token=AU6BUZSBD4TVJR3HIR26COLBRZWLW')
+link2 = 'https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Christophe/film3.csv?token=AVCI5TZAWZBXSYRTV5AZ3MLBSVAIK'
+film = pd.read_csv(link2)
+link3 = 'https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Christophe/concat_liste50.csv?token=AVCI5TZTGITFN6FQM3LD5L3BSVB7K'
+link4 = 'https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Christophe/concat_listeTopFilm.csv?token=AVCI5T2WNH57SGCWSMYSBW3BSVCC4'
+link5 = 'https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Christophe/concat_listeTopTV.csv?token=AVCI5T3UN6DRCDNB4TLK5WLBSVCFQ'
+concat_liste_50 = pd.read_csv(link3)
+concat_listeTopFilm = pd.read_csv(link4)
+concat_listeTopTV = pd.read_csv(link5)
 
+fig = px.bar(presence_acteur, x="primaryName", y ='index', color = 'index',
+    title = 'Quels sont les acteurs les plus présents ?',
+    labels = {'primaryName': 'Nombre de films', 'index': 'Acteurs'},
+    width=800, height=600)
+
+fig.update_layout(showlegend=False, title_x=0.5, yaxis={'visible': True})
+
+st.plotly_chart(fig)
 
 fig = px.bar(acteur_par_periode, x = 'count', y="rank", text ='primaryName', color = 'primaryName',
     title = 'Quels sont les acteurs les plus présents par périodes ?',
