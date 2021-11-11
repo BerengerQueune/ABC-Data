b838a8b7c427b47e7286c6a0f2e075b5ebe3f4dc
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-11 22:19:18 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -70,11 +70,6 @@ test5 = px.bar(top10, x='Pays', y='Nb de films', color="Nb de films", color_cont
 
 st.plotly_chart(test5)
 
-test1 = px.line(film, x='startYear', y = 'runtimeMinutes', title = 'Evolution de la durée des films depuis 1960', labels=dict(startYear="Année", runtimeMinutes="Durée en minutes"), 
-        width=600, height=400, color_discrete_sequence = ['red'])
-
-st.plotly_chart(test1)
-
 
 fig = make_subplots(rows=2, cols=2)
 
@@ -109,24 +104,10 @@ fig.update_yaxes(title_text="Minutes", row=2, col=2, range=[0, 100])
 
 fig.update_layout(height=1000, width=1400, title_text="Evolution de la durée des films depuis 1960", title_x=0.5)
 
+
+
 st.plotly_chart(fig)
 
-
-
-test2 = px.line(film, x='startYear', y = 'runtimeMinutes', title = 'Evolution de la durée des films depuis 1960', labels=dict(startYear="Année", runtimeMinutes="Durée en minutes"), 
-        width=600, height=400, range_y=(80,100), color_discrete_sequence = ['green'])
-
-st.plotly_chart(test2)
-
-test3 = px.line(film, x='startYear', y = 'runtimeMinutes', title = 'Evolution de la durée des films depuis 1960', labels=dict(startYear="Année", runtimeMinutes="Durée en minutes"), 
-        width=600, height=400, range_y=(50,100), color_discrete_sequence = ['blue'])
-
-st.plotly_chart(test3)
-
-test4 = px.line(film, x='startYear', y = 'runtimeMinutes', title = 'Evolution de la durée des films depuis 1960', labels=dict(startYear="Année", runtimeMinutes="Durée en minutes"), 
-        width=600, height=400, range_y=(0,100), color_discrete_sequence = ['orange'])
-
-st.plotly_chart(test4)
 
 fig = px.bar(data_frame = concat_liste_50, x= "primaryName", y="nb", color = 'type', color_discrete_sequence=["darkred", "green"],labels=dict(primaryName="Nom de l'acteur", nb="Nombre de films"))
 fig.update_layout(title_text="Top 20 des acteurs ayant tournés autant au cinéma qu'à la TV", width=1000, height=600)
