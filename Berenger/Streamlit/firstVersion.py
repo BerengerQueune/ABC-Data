437b2e6a4ffbed78e8508658a7b8582adee0f6a3
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-13 15:55:10 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -16,100 +16,60 @@ df_recommandation = pd.read_csv('https://raw.githubusercontent.com/BerengerQueun
 #st.set_page_config( layout='wide')
 
 
-# def main():
+def main():
 
-#     st.title("TEST")
-#     menu = ["Home", "Search", "About"]
+    st.title("TEST")
+    menu = ["Home", "Search", "About"]
 
-#     choice = st.sidebar.selectbox("Menu", menu)
+    choice = st.sidebar.selectbox("Menu", menu)
 
-#     df = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Database_projet/df_base.csv?token=AU6BUZR7RH7QL3XQCYCQ2P3BS53T4')
+    df = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Database_projet/df_base.csv?token=AU6BUZR7RH7QL3XQCYCQ2P3BS53T4')
 
 
 
-#     if choice == 'Home':
-#         st.subheader("Home")
+    if choice == 'Home':
+        st.subheader("Home")
 
-#         # with st.expander("Title"):
-#         #     mytext = st.text_area("Type Here")
-#         #     st.write(mytext)
-#         #     st.success("Hello")
+        # with st.expander("Title"):
+        #     mytext = st.text_area("Type Here")
+        #     st.write(mytext)
+        #     st.success("Hello")
 
-#         #st.dataframe(df)
-#         movies_title_list = df["primaryTitle"].tolist()
+        #st.dataframe(df)
+        movies_title_list = df["primaryTitle"].tolist()
 
-#         movie_choice = st.selectbox("Movie Title", movies_title_list)
-#         with st.expander('Movies DF'):
-#             st.dataframe(df.head(10))
+        movie_choice = st.selectbox("Movie Title", movies_title_list)
+        with st.expander('Movies DF'):
+            st.dataframe(df.head(10))
 
-#             # Filter
-#             # img_link = df[df["primaryTitle"] == movie_choice]["img_link"].values[0]
-#             # title_link = df[df["primaryTitle"] == movie_choice]["primaryTitle"].values
-#             genre = df[df["primaryTitle"] == movie_choice]["Comedy"].values
+            # Filter
+            # img_link = df[df["primaryTitle"] == movie_choice]["img_link"].values[0]
+            # title_link = df[df["primaryTitle"] == movie_choice]["primaryTitle"].values
+            genre = df[df["primaryTitle"] == movie_choice]["Comedy"].values
 
-#         #Layout
-#         # st.write(img_link)
-#         # st.image(img_link)
-#         c1 = st.columns(1)
+        #Layout
+        # st.write(img_link)
+        # st.image(img_link)
+        col1, col2  = st.columns(2)
 
-#         # with c1:
-#         #     with st.expander("primaryTitle"):
-#         #         st.write(genre)
+        with col1:
+	        fname = st.text_input("Enter your first name")
 
-#         with c1:
-#             st.write("lol")
+        with col2:
+	        lname = st.text_input("Enter your Last name")
     
 
     
 
 
 
-#     elif choice == "Search":
-#         st.subheader("Search By Year")
+    elif choice == "Search":
+        st.subheader("Search By Year")
 
 
-#     else:
-#         st.subheader("About")
+    else:
+        st.subheader("About")
 
-# main()
-
-country = ["USA", "India", "Canada", "UK", "Australia"]
-st.selectbox("Country", country)
-
-col1, col2  = st.columns(2)
-
-with col1:
-	fname = st.text_input("Enter your first name")
-
-with col2:
-	lname = st.text_input("Enter your Last name")
-
-
-st.text_area("Enter your Address")
-
-city, state, zc = st.columns([4,3,2])
-
-with city:
-	c = st.text_input("City")
-
-with state:
-	s = st.text_input("State")
-
-with zc:
-	z = st.text_input("ZipCode")
-
-
-mail, pn = st.columns([3,1])
-
-with mail:
-	email = st.text_input("Enter your Email")
-
-with pn:
-	phone = st.text_input("Enter your Phone Number")
-
-
-#st.button("Save")
-if st.button("Save"):
-	st.success("Your Information was saved successfully!!")
+main()
 
 
