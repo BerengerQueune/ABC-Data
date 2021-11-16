949b72d92657e4cfcf2b58c3f3d3fcfdf11a52e4
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-16 17:33:32 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -104,7 +104,7 @@ def main():
             if i in df["primaryTitle"]:
                 st.write("success", i)
 
-        st.write(options.info())
+        st.write(options.columns
 
     
     elif choice == "test":
