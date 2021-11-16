c3030ce89585d411259c1cd43c15df5adc015504
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-16 17:13:26 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -14,7 +14,7 @@ from sklearn.neighbors import NearestNeighbors
 df_recommandation = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Database_projet/df_recommendation.csv?token=AU6BUZUA5UESEPKRRJQIESLBS53UU')
 df = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Database_projet/df_base.csv?token=AU6BUZWHN456IAMFBUWFFSDBTELCU')
 
-st.set_page_config( layout='wide')
+#st.set_page_config( layout='wide')
 
 
 def main():
