e8f775209499dccbc620c3aef9d5bb22390b9c0e
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-16 18:47:19 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -65,10 +65,14 @@ def main():
 
         liste_finale = df_recommandation.iloc[mewtwo]
 
-        st.write('Nous vous proposons les films suivants :')
+        if len(liste_finale) == 0:
+            pass
+        else:
 
-        for i in range(len(liste_finale)):
-            st.write("- ", liste_finale.iloc[i]["primaryTitle"])
+            st.write('Nous vous proposons les films suivants :')
+
+            for i in range(len(liste_finale)):
+                st.write("- ", liste_finale.iloc[i]["primaryTitle"])
     
     elif choice == "test":
         options = st.multiselect(
