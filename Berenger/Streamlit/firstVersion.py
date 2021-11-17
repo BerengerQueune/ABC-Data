2a1ee9b139310e91b00c9907fdd8b69e70f27c77
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 11:12:28 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -68,13 +68,14 @@ def main():
         movies = 0
 
         st.write(liste_finale)
-        if movies == 0:
-            pass
-        else:
-            st.write('Nous vous proposons les films suivants :')
 
-            for i in range(len(liste_finale)):
-                st.write("- ", liste_finale.iloc[i]["primaryTitle"])
+        if True in mask_countries:
+            print ("True")
+
+        st.write('Nous vous proposons les films suivants :')
+
+        for i in range(len(liste_finale)):
+            st.write("- ", liste_finale.iloc[i]["primaryTitle"])
     
     elif choice == "test":
         options = st.multiselect(
