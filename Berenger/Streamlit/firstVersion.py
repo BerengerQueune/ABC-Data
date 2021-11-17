5f1aa83512b6319514a0158ff4a294cdec7d58ab
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 22:33:53 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -53,7 +53,10 @@ def main():
 
         st.write("---------------------------------------------------------")
 
-
+        if st.button('Say hello'):
+            st.write('Why hello there')
+        else:
+            st.write('Goodbye')
 
 
 
