0f00c77005123f2abe0c5f8a7e9a7c9c9189018f
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 17:00:28 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -112,8 +112,8 @@ def main():
                     series = ia.get_movie(code)
                     try:
                         # # getting cover url of the series
-                        cover = series.data['cover url']
-                        url = series['cover']
+                        url  = series['cover url']
+                        url = url[:url.rindex('@') + 1] + url[url.rindex('.'):]
                         # # print the cover
                         st.image(url_clean(url), width =300, caption=movie_name)
                     except KeyError:
