d548b5d034bad74cffc6b1cff05daef68aa5a047
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 22:11:01 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -43,6 +43,13 @@ def main():
 
             st.subheader("Système de recommandation")
             st.markdown(hide_menu, unsafe_allow_html=True)
+            COUNTRIES = df['primaryTitle'].unique()
+            COUNTRIES_SELECTED = st.multiselect('Choisissez vos films préférés :', COUNTRIES)
+
+            # Mask to filter dataframe
+            mask_countries = df['primaryTitle'].isin(COUNTRIES_SELECTED)
+
+            data = df[mask_countries]
         with c3:
             st.write("")
 
