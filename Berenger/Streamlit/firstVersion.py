31c3224a05fb53e6f7deaef5e4fc4bc0d316f3b8
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 22:31:58 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -29,8 +29,9 @@ st.set_page_config(page_title="ABCS", page_icon=":heart:", layout='wide')
 st.markdown("<h1 style='text-align: center; font-family:cursive; color: white;'>Projet recommandation de films de l'équipe ABCS</h1>", unsafe_allow_html=True)
 
 def main():
-
-    
+    def lol():
+        st.write("LOOOOL")
+    st.button(on_click=lol())
 
 
     menu = ["Système de recommandation", "Meaningful KPI"]
