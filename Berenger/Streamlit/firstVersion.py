e0472e3bf0ff0dc6d15b32c708238e57dfbd88ed
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 22:20:31 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -42,7 +42,7 @@ def main():
         with c2:
 
             st.subheader("Système de recommandation")
-            st.markdown(hide_menu, unsafe_allow_html=True)
+            st.markdown(hide_menu)
         with c3:
             st.write("")
 
