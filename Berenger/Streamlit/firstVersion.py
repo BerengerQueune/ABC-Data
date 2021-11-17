ced46508ffd44dabbf5bb5ce9e5dc7ea33fe5e95
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-17 21:26:54 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -97,7 +97,7 @@ def main():
                 
                 
                 with cols[numero_colonne]:
-                    movie_name = liste_finale.iloc[i]["startYear"]
+                    movie_name = liste_finale.iloc[i]["primaryTitle"]
                     # id
                     code = liste_finale.iloc[i]["tconst"]
                     code = code.replace("tt", "")
