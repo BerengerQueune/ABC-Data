d1d573abd6bec28448784d54edd1421f1b435c0f
BerengerQueune <87824998+BerengerQueune@users.noreply.github.com>
2021-11-18 11:19:28 +0100

    Update firstVersion.py

--- Berenger/Streamlit/firstVersion.py
+++ Berenger/Streamlit/firstVersion.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 from sklearn.neighbors import NearestNeighbors
 import imdb
 import imdb.helpers
